f53c71381ddf957039f443d557137f45362d8d28
tomamishev <47216444+tomamishev@users.noreply.github.com>
2022-09-24 17:07:40 +0300

    Add files via upload

---
+++ Day 5/Excercises/high_score.py
@@ -0,0 +1,19 @@
+# You are going to write a program that calculates the highest score from a List of scores.
+# e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+# Important you are not allowed to use the max or min functions. The output words must match the example. i
+#
+# The highest score in the class is: x
+# 🚨 Don't change the code below 👇
+import sys
+
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+print(f"The highest score in the class is: {max_score}")
